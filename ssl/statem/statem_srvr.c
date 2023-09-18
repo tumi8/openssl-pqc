adb54f315d45496f53b83c54eec0c27d52433775
Markus Sosnowski <sosnowski@net.in.tum.de>
2023-09-18 18:10:07 +0200

    TCP Push work

--- ssl/statem/statem_srvr.c
+++ ssl/statem/statem_srvr.c
@@ -841,10 +841,10 @@ WORK_STATE ossl_statem_server_post_work(SSL *s, WORK_STATE wst)
         break;
 
     case TLS_ST_SW_SRVR_HELLO:
-#ifdef NOKAI_EARLY_PSH
+//#ifdef NOKAI_EARLY_PSH
         if (statem_flush(s) != 1)
             return WORK_MORE_A;
-#endif
+//#endif
 
         if (SSL_IS_TLS13(s) && s->hello_retry_request == SSL_HRR_PENDING) {
             if ((s->options & SSL_OP_ENABLE_MIDDLEBOX_COMPAT) == 0
@@ -941,9 +941,9 @@ WORK_STATE ossl_statem_server_post_work(SSL *s, WORK_STATE wst)
             dtls1_reset_seq_numbers(s, SSL3_CC_WRITE);
         break;
 
-#ifdef NOKAI_EARLY_PSH
+// #ifdef NOKAI_EARLY_PSH
     case TLS_ST_SW_CERT:
-#endif
+// #endif
     case TLS_ST_SW_SRVR_DONE:
         if (statem_flush(s) != 1)
             return WORK_MORE_A;
